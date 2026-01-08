c3483b90dcc3db371fef4ac15527046959c6f5b8
Axel Eration <pereyrol.axel@gmail.com>
2026-01-08 23:53:38 +0100

    edit du script python

--- scripts/extract_vertices.py
+++ scripts/extract_vertices.py
@@ -1,13 +1,20 @@
 import bpy
 
+print("script en cours de fonctionnement")
+
 obj = bpy.context.active_object
-mesh = obj.data
 
-# Passe en mode objet pour éviter les bugs
-bpy.ops.object.mode_set(mode='OBJECT')
+#test sur l'objet
+if obj is None:
+    print("ERREUR : Aucun objet sélectionné")
+elif obj.type != 'MESH':
+    print(f"ERREUR : L'objet sélectionné n'est pas un mesh ({obj.type})")
+else:
+    mesh = obj.data
+    bpy.ops.object.mode_set(mode='OBJECT')
 
-print("float vertices[] = {")
-for v in mesh.vertices:
-    x, y, z = v.co
-    print(f"    {x:.6f}f, {y:.6f}f, {z:.6f}f,")
-print("};")
+    print("float vertices[] = {")
+    for v in mesh.vertices:
+        x, y, z = v.co
+        print(f"    {x:.6f}f, {y:.6f}f, {z:.6f}f,")
+    print("};")
